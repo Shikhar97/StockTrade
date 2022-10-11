250feaf9d5b5055871525bea9c7f0c3d7c3b874d
Sara <saragupta1924@gial.com>
2022-10-12 00:41:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+bootstrap = Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route("/") 
+def index():
+     return render_template("index.html")
+
+
+@app.route("/userlogin",methods=['GET','POST']) 
+def userlogin():  
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+    return render_template('user_login.html',form=form)
+
+
+
+@app.route("/usersignup",methods=['GET','POST']) 
+def usersignup():
+    form= RegisterForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+    return render_template('user_signup.html',form=form)
+
+    
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route("/admin") 
+def adminpage():
+     return render_template("admin.html")
+   
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
